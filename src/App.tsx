86630ec6057f9e9e9062f88dd8adee6712086376
Fred Hajdarpasic <fredh@supporterhub.com>
2021-07-31 09:56:37 +1000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Render from './Render';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Render />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
